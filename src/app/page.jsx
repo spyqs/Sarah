87f2dbbba2d0923d49d933f3f39118296fc01d01
spyqs <spyqsklar@gmail.com>
2024-08-13 23:57:29 +0000

    repl.it test from sarah's computer

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -180,7 +180,7 @@ export default async function Home() {
       <Container className="mt-24 sm:mt-32 md:mt-56">
         <FadeIn className="max-w-3xl">
           <h1 className="font-display text-5xl font-medium tracking-tight text-neutral-950 [text-wrap:balance] sm:text-7xl">
-            Sarah's consulting services.
+            Ashby Research. Getting shit done.
           </h1>
           <p className="mt-6 text-xl text-neutral-600">
             We are a development studio working at the intersection of design
